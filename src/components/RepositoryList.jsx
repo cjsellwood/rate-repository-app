7bbb48eb3b9e589603f6e3122d90c01c9a6afc86
cjsellwood <c.sellwood22@gmail.com>
2021-08-10 11:05:22 +1000

    10.23 complete

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,18 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlatList, View, StyleSheet, Pressable } from "react-native";
 import RepositoryItem from "./RepositoryItem";
 import useRepositories from "../hooks/useRepositories";
 import { useHistory } from "react-router-native";
+import Text from "./Text";
+import { Menu, Provider } from "react-native-paper";
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
   },
+  order: {
+    padding: 20,
+    fontSize: 16,
+  },
+  menuItem: {},
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories }) => {
+const TopOptions = ({ order, setOrder }) => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <Menu
+          visible={visible}
+          onDismiss={() => setVisible(false)}
+          anchor={
+            <Pressable onPress={() => setVisible(true)}>
+              <Text style={styles.order}>{order} &#9660;</Text>
+            </Pressable>
+          }
+        >
+          <Menu.Item
+            style={styles.menuItem}
+            onPress={() => {
+              setOrder("Latest repositories");
+              setVisible(false);
+            }}
+            title="Latest repositories"
+          />
+          <Menu.Item
+            onPress={() => {
+              setOrder("Highest rated repositories");
+              setVisible(false);
+            }}
+            title="Highest rated repositories"
+          />
+          <Menu.Item
+            onPress={() => {
+              setOrder("Lowest rated repositories");
+              setVisible(false);
+            }}
+            title="Lowest rated repositories"
+          />
+        </Menu>
+      </View>
+    </View>
+  );
+};
+
+export const RepositoryListContainer = ({ repositories, order, setOrder }) => {
   const history = useHistory();
 
   // Get the nodes from the edges array
@@ -25,24 +79,34 @@ export const RepositoryListContainer = ({ repositories }) => {
   };
 
   return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={({ item }) => {
-        return (
-          <Pressable onPress={() => toRepository(item.id)}>
-            <RepositoryItem item={item} />
-          </Pressable>
-        );
-      }}
-    />
+    <Provider>
+      <FlatList
+        data={repositoryNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        ListHeaderComponent={<TopOptions order={order} setOrder={setOrder} />}
+        renderItem={({ item }) => {
+          return (
+            <Pressable onPress={() => toRepository(item.id)}>
+              <RepositoryItem item={item} />
+            </Pressable>
+          );
+        }}
+      />
+    </Provider>
   );
 };
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const [order, setOrder] = useState("Latest Repositories");
+  const { repositories } = useRepositories(order);
 
-  return <RepositoryListContainer repositories={repositories} />;
+  return (
+    <RepositoryListContainer
+      repositories={repositories}
+      order={order}
+      setOrder={setOrder}
+    />
+  );
 };
 
 export default RepositoryList;
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -2,10 +2,29 @@ import { useQuery } from "@apollo/client";
 import { useState, useEffect } from "react";
 import { GET_REPOSITORIES } from "../graphql/queries";
 
-const useRepositories = () => {
+const useRepositories = (order) => {
   const [repositories, setRepositories] = useState();
 
+  let variables = {};
+  switch (order) {
+    case "Latest repositories":
+      variables.orderBy = "CREATED_AT";
+      variables.orderDirection = "DESC";
+      break;
+    case "Highest rated repositories":
+      variables.orderBy = "RATING_AVERAGE";
+      variables.orderDirection = "DESC";
+      break;
+    case "Lowest rated repositories":
+      variables.orderBy = "RATING_AVERAGE";
+      variables.orderDirection = "ASC";
+      break;
+    default:
+      break;
+  }
+
   const result = useQuery(GET_REPOSITORIES, {
+    variables,
     fetchPolicy: "cache-and-network",
   });
 
